12b386c292bd5edc7f19a636721cf992440f7c12
Dhineshwaran <dhineshwaran02@gmail.com>
2022-10-25 23:26:26 +0530

    Creating the project 3 on Oct 25th

---
+++ project_3/dw-1/src/Components/Sidebar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import TollIcon from '@mui/icons-material/Toll';
+import InsertCommentIcon from '@mui/icons-material/InsertComment';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import "./Sidebar.css"
+function Sidebar() {
+  return (
+    <div className='sidebar'>
+        <div className='sidebar-header'>
+            <div className='sidebar-header-img'>
+                <img src='./user.png' alt='' />
+            </div>
+            <div className='sidebar-header-btn'>
+                <TollIcon />
+                <InsertCommentIcon />
+                <MoreVertIcon />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
