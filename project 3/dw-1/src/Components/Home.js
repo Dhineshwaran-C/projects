39b08f0f82241454b4395cebc94c0eb123232dbb
Dhineshwaran <dhineshwaran02@gmail.com>
2022-10-28 23:01:58 +0530

    Updated On Oct 28th

--- project 3/dw-1/src/Components/Home.js
+++ project 3/dw-1/src/Components/Home.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Sidebar from './Sidebar'
-
+import './Home.css'
 function Home() {
   return (
     <div className='home'>
-        <div className='container'>
+        <div className='home-container'>
             <Sidebar/>
+            <div className='home-bg'>
+
+            </div>
         </div>
     </div>
   )
--- project 3/dw-1/src/Components/Sidebar.js
+++ project 3/dw-1/src/Components/Sidebar.js
@@ -2,13 +2,41 @@ import React from 'react'
 import TollIcon from '@mui/icons-material/Toll';
 import InsertCommentIcon from '@mui/icons-material/InsertComment';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import SearchIcon from '@mui/icons-material/Search';
 import "./Sidebar.css"
+import UserProfile from './UserProfile';
 function Sidebar() {
   return (
     <div className='sidebar'>
+        
+        <div className='sidebar-search'>
+          <div className='sidebar-search-input'>
+            <SearchIcon />
+            <input type='text' name='search' placeholder='Search...' />
+          </div>
+        </div>
+        <div className='sidebar-chat-list'>
+          <UserProfile name='Dhinesh' photoURL='./user.png'/>
+          <UserProfile name='Shyam' photoURL='./user.png'/>
+          <UserProfile name='Bhuvanesh' photoURL='./user.png'/>
+          <UserProfile name='Dhinesh' photoURL='./user.png'/>
+          <UserProfile name='Dhinesh' photoURL='./user.png'/>
+          <UserProfile name='Dhinesh' photoURL='./user.png'/>
+          <UserProfile name='Dhinesh' photoURL='./user.png'/>
+          <UserProfile name='Dhinesh' photoURL='./user.png'/>
+          <UserProfile name='Dhinesh' photoURL='./user.png'/>
+          <UserProfile name='Dhinesh' photoURL='./user.png'/>
+          <UserProfile name='Dhinesh' photoURL='./user.png'/>
+          <UserProfile name='Dhinesh' photoURL='./user.png'/>
+          <UserProfile name='Dhinesh' photoURL='./user.png'/>
+          <UserProfile name='Dhinesh' photoURL='./user.png'/>
+        </div>
         <div className='sidebar-header'>
             <div className='sidebar-header-img'>
                 <img src='./user.png' alt='' />
+            </div>
+            <div className='sidebar-header-name'>
+              <p className='user-name'>Name</p>
             </div>
             <div className='sidebar-header-btn'>
                 <TollIcon />
